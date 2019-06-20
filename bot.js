1493e0d77c09fdf86f1306d839f39c9096ef91c2
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-06-20 14:30:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,38 +28,82 @@ client.on('ready', () => {
   console.log('')
 });
  
-client.on('message', message => {
-    var p = message.mentions.members.first();
-    var reason = message.content.split(" ").slice(2).join(' ');
-    var log = message.guild.channels.find('name', 'warns-log');
-    if(message.content.startsWith(`${prefix}warn`)){
-        if(!p) return message.reply(`**منشن الشخص اول**`);
-        if(!reason) return message.reply(`**حط سبب**`);
-        if(!p.bannable) return message.reply(`**مقدر اعطي وورن لشخص من الادارة**`);
-        reason = reason.replace('1', "**كتابة الاوامر بالشات العام**");
-        reason = reason.replace('2', "**بيع اشياء**");
-        reason = reason.replace('3', "**التحذث عن السياسة**");
-        reason = reason.replace('4', "**التحذث عن الدين **");
-        reason = reason.replace('5', "**التحدث عن الطائفية**");
-        reason = reason.replace('6', "**السبام**");
-        reason = reason.replace('7', "**فتح تذكرة من دون سبب**");
-        reason = reason.replace('8', "**العنصرية**");
-        reason = reason.replace('9', "**عدم الاحترام**");
-        reason = reason.replace('10', "**نشر بالعام**");
-        var embed = new Discord.RichEmbed()
-        .setAuthor(`تم التحذير`)
-        .addField(`Name ♣`, `<@${p.id}>`)
-        .addField(`By ♣`, `<@${message.author.id}>`)
-        .addField(`Reason ♣`, reason)
-        .setTimestamp()
-        .setColor("WHITE")
-        .setFooter(` `)
-        message.channel.send(`${p} ${reason}`)
-            message.delete();
-        log.send({embed});
-        warnRoles = ['Only Me']
+client.on('message', (message) => {
+    if(message.author.bot) return;
+    if(message.content.startsWith(prefix + 'quran')) {
+        let codes = message.content.split(' ').slice(1);
+        let num;
+        if(!codes[0] || isNaN(codes[0])) num = 1
+        else num = parseInt(codes[0])
+       
+        // Embed Of Quran
+        let embed = new Discord.RichEmbed()
+        .setAuthor("Quran | القرآن", client.user.displayAvatarURL)
+        .setTitle("صفحات القرآن الكريم")
+        .setImage(getURLCodes(num))
+        .setFooter(getFooterCodes(num))
+        // Reactions
+        let l = '⬅';
+        let p = '⏹';
+        let r = '➡';
+        // Filters
+        let lF = (reaction, user) => reaction.emoji.name == l && user.id == message.author.id;
+        let pF = (reaction, user) => reaction.emoji.name == p && user.id == message.author.id;
+        let rF = (reaction, user) => reaction.emoji.name == r && user.id == message.author.id;
+        message.channel.send(embed).then(async msg => {
+            await msg.react(r)
+            await msg.react(p)
+            await msg.react(l)
+            // Collecters
+            let lC = msg.createReactionCollector(lF)
+            let pC = msg.createReactionCollector(pF)
+            let rC = msg.createReactionCollector(rF)
+            lC.on('collect', codes => {
+                if(num == 604) return;
+                num++;
+                embed.setImage(getURLCodes(num))
+                embed.setFooter(getFooterCodes(num))
+                msg.reactions.forEach(reaction => {
+                    reaction.fetchUsers().then(usersCodes => {
+                        usersCodes.forEach(user => {
+                            if(user.bot) return;
+                            reaction.remove(user)
+                        })
+                    })
+                })
+                msg.edit(embed)
+            })
+            pC.on('collect', codes => {
+                message.channel.send("سيتم اغلاق المصحف خلال 5 ثواني").then(codes => {
+                    codes.delete(5000)
+                    msg.delete(5000)
+                    message.delete(5000)
+                })
+            })
+            rC.on('collect', codes => {
+                if(num == 1) return;
+                num--;
+                embed.setImage(getURLCodes(num))
+                embed.setFooter(getFooterCodes(num))
+                msg.reactions.forEach(reaction => {
+                    reaction.fetchUsers().then(users => {
+                        users.forEach(user => {
+                            if(user.bot) return;
+                            reaction.remove(user)
+                        })
+                    })
+                })
+                msg.edit(embed)
+            })
+        })
     }
-});
+})
+function getURLCodes(num) {
+    return `http://quran.ksu.edu.sa/ayat/safahat1/${num}.png`
+}
+function getFooterCodes(num) {
+    return `الصفحة رقم ${num} من أصل 604 صفحة`
+}
 
 
-client.login("NTc5NTkyMTE3OTcwNjY1NDc1.XP5qoA.zAx0FrRgq4HRrU_cksQeHrvDKs0");
+client.login(process.env.BOT_TOKEN);
